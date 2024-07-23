4e68dbd3531536126fe7187c0cbce51f2d349322
WOOJIN LEE <127282094+cyberhorizon@users.noreply.github.com>
2024-07-23 21:17:16 +0900

    계산기능 공부한것

---
+++ caculator.java
@@ -0,0 +1,14 @@
+package ch01.sec09;
+
+public class caculator {
+
+	public static void main(String[] args) {
+		
+		int x=48;              //변수x 값을 48로 지정
+  		int y=3;               //변수y 값을 3로 지정
+  		int z=9;               //변수z 값을 9로 지정
+		int result=x*y/z;      //변수 x값과 y값을 곱하고 z값으로 나눠라 
+    System.out.println(result);
+	}
+
+}

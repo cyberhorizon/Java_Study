a9010d4e8c8497b9a6c7a5adf4ac2f2a47bb9442
WOOJIN LEE <127282094+cyberhorizon@users.noreply.github.com>
2024-07-23 21:16:49 +0900

    자바 공부 다시 시작한날

---
+++ hello.java
@@ -0,0 +1,16 @@
+package ch01.sec09;
+/**
+ * @author WooJin
+ */
+/*
+ 장 제목 1장 자바 시작하기
+작성일:2024.04.30
+ */
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hello,java");
+	}
+
+}

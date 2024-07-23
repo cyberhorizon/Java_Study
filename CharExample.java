c5ce368cef20a0b9effd0a4d142a9383cb2a12b2
WOOJIN LEE <127282094+cyberhorizon@users.noreply.github.com>
2024-07-23 21:20:48 +0900

    유니코드

---
+++ CharExample.java
@@ -0,0 +1,20 @@
+package ch02.sec03;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		char c1='A';//문자 지정
+		char c2=40;//유니코드 번호
+		
+		char c3='B';//문자 지정
+		char c4=44032;//유니코드
+		
+		System.out.println("c1:"+c1);
+		System.out.println("c2:"+c2);
+		System.out.println("c3:"+c3);
+		System.out.println("c4:"+c4);
+				
+		
+	}
+
+}
